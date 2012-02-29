69244db277348b1a61fa75fcf0780c709aca9b68
Barry O'Donovan <barry@opensolutions.ie>
2012-02-29 09:52:17 +0000

    Version bump

--- library/ViMbAdmin/Version.php
+++ library/ViMbAdmin/Version.php
@@ -46,7 +46,7 @@ final class ViMbAdmin_Version
     /**
      * Version identification - see compareVersion()
      */
-    const VERSION = '2.0.2';
+    const VERSION = '2.0.3';
 
     /**
      * The latest stable version Zend Framework available

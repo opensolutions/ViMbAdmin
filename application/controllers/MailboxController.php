d6d47a192cb2e516856e1b73d630ad09f0b651dd
Dustin Farris <dustin@dustinfarris.com>
2012-05-07 23:04:25 -0400

    Fixing what was probably a bad typo.

--- application/controllers/MailboxController.php
+++ application/controllers/MailboxController.php
@@ -470,12 +470,15 @@ class MailboxController extends ViMbAdmin_Controller_Action
                         {
                             $this->_mailbox['quota'] = $this->_domain['quota'];
                             $this->addMessage(
-                                _( "Mailbox qif( !$this->_getParam( 'helper', true ) )
-                    $this->view->modal = trueuota set to " ) . $this->_domain['quota'],
-                                ViMbAdmin_Message::ALERT
-                            );
-                        }
-                    }
+								_("Mailbox quota set to ") . $this->_domain['quota'],
+								ViMbAdmin_Message::ALERT
+							);
+						}
+					}
+                    if(!$this->_getParam('helper', true))
+					{
+                    	$this->view->modal = true;
+					}
 
                     if( isset( $this->_options['allow_access_restriction'] ) && $this->_options['allow_access_restriction'] )
                     {

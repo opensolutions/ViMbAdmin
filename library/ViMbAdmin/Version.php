fdc5e71cd7f3a125b1ac7a2b4c99c6e91756e1fd
Barry O'Donovan <barry@opensolutions.ie>
2014-02-17 10:32:38 +0000

    Version bump

--- library/ViMbAdmin/Version.php
+++ library/ViMbAdmin/Version.php
@@ -46,7 +46,7 @@ final class ViMbAdmin_Version
     /**
      * Version identification - see compareVersion()
      */
-    const VERSION = '3.0.2';
+    const VERSION = '3.0.3';
 
     /**
      * Database schema version

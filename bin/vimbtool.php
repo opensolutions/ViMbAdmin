180e76f50171a2726421a5c5b33a48061a7f1a30
Phiber2000 <phiber2000@gmx.de>
2016-04-20 14:04:52 +0200

    removed default timezone in vimbtool (always overwrote CLIs php.ini)

--- bin/vimbtool.php
+++ bin/vimbtool.php
@@ -5,7 +5,6 @@
  * CLI script
  */
 
-date_default_timezone_set('Europe/Dublin');
 require_once( dirname( __FILE__ ) . '/../vendor/autoload.php' );
 require_once( dirname( __FILE__ ) . '/utils.inc' );
 //define( 'APPLICATION_ENV', scriptutils_get_application_env() );

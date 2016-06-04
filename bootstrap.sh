de6df40e375cdd64b2a1368682800727b7158beb
Barry O'Donovan <barry@opensolutions.ie>
2016-06-04 22:06:14 +0100

    Add php-memcache

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ apt full-upgrade -y
 
 apt install -y apache2 php7.0 php7.0-intl php7.0-mysql php-rrd php7.0-cgi php7.0-cli php7.0-curl php7.0-mcrypt \
     php-memcached libapache2-mod-php7.0 mysql-server mysql-client php-mysql joe memcached      \
-    php7.0-mbstring php7.0-xml phpmyadmin php-gettext screen joe
+    php7.0-mbstring php7.0-xml phpmyadmin php-gettext screen joe php-memcache
 
 if ! [ -L /var/www ]; then
   rm -rf /var/www

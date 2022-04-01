6fc7b6a7be3eb9264b27caf4e48d8c90bafc3ce9
Barry O'Donovan <barry@opensolutions.ie>
2022-04-01 21:59:21 +0100

    Bumped version number to 3.3.0

--- library/ViMbAdmin/Version.php
+++ library/ViMbAdmin/Version.php
@@ -46,7 +46,7 @@ final class ViMbAdmin_Version
     /**
      * Version identification - see compareVersion()
      */
-    const VERSION = '3.2.1';
+    const VERSION = '3.3.0';
 
     /**
      * Database schema version
